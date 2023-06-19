c136d66813abaae79aef193dd555f77d33c7ea5f
gamingnotice <64684810+gamingnotice@users.noreply.github.com>
2023-06-19 15:24:48 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.AdminOnly = true 
+Config.MaxNPCs = 2 
+
+Config.NotifySystem = 'vms_notify' 
+Config.NotifyEvent = 'Notification'
+
+Config.Messages = {
+    NPCSpawnError = "Sie haben bereits die maximale Anzahl an NPCs erstellt.",
+    CreateNPCPrompt = "Bitte geben Sie den Namen des NPCs ein.",
+    NPCFollowSuccess = "NPC(s) folgen dir jetzt.",
+    NoFreeSeat = "Kein freier Sitzplatz im Fahrzeug!",
+    NotInVehicle = "Du bist in keinem Fahrzeug!",
+}
